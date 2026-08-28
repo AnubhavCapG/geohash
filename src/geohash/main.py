ab9b4f5fba689a515b5fe94a25bf91ac98adfbdd
AnubhavCapG <anubhav.b.sharma@capgemini.com>
2026-08-28 17:12:06 +0530

    Corrected the Geohash implementation

--- src/geohash/main.py
+++ src/geohash/main.py
@@ -1,5 +1,5 @@
 def base32(number: int) -> str:
-    alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUV"
+    alphabet = "0123456789bcdefghjkmnpqrstuvwxyz"
     if number == 0:
         return "0"
     result = ""
@@ -13,23 +13,24 @@ def main(precision: int = 10) -> None:
     user_lat, user_long = 12.973343525803152, 77.71789536074326
     lat_lower_bound, lat_upper_bound = -90, 90
     long_lower_bound, long_upper_bound = -180, 180
-    lat_encoding, long_encoding = [], []
-    for _ in range(precision):
-        lat_mid = (lat_lower_bound + lat_upper_bound) / 2
-        long_mid = (long_lower_bound + long_upper_bound) / 2
-        if user_lat >= lat_mid:
-            lat_encoding.append(1)
-            lat_lower_bound = lat_mid
+    alternating_encoding = []
+    for i in range(precision * 5):
+        if i % 2 == 0:
+            long_mid = (long_lower_bound + long_upper_bound) / 2
+            if user_long >= long_mid:
+                alternating_encoding.append(1)
+                long_lower_bound = long_mid
+            else:
+                alternating_encoding.append(0)
+                long_upper_bound = long_mid
         else:
-            lat_encoding.append(0)
-            lat_upper_bound = lat_mid
-        if user_long >= long_mid:
-            long_encoding.append(1)
-            long_lower_bound = long_mid
-        else:
-            long_encoding.append(0)
-            long_upper_bound = long_mid
-    alternating_encoding = [item for pair in zip(long_encoding, lat_encoding) for item in pair]
+            lat_mid = (lat_lower_bound + lat_upper_bound) / 2
+            if user_lat >= lat_mid:
+                alternating_encoding.append(1)
+                lat_lower_bound = lat_mid
+            else:
+                alternating_encoding.append(0)
+                lat_upper_bound = lat_mid
     built_chunks = [alternating_encoding[i:i+5] for i in range(0, len(alternating_encoding), 5)]
     connected_chunks = ["".join(map(str, chunk)) for chunk in built_chunks]
     geohash_binary_as_decimal = [int(chunk, 2) for chunk in connected_chunks]
